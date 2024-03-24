6a2c27007c989b3abca52a7fc820340dd335e8fb
Damilola Okunlola <okunlolatawakalitu61@gmail.com>
2024-03-24 22:46:53 +0100

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{html, js}'],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      'xmd': "980px",
+      lg: "1024px",
+      xl: "1120px",
+      "2xl": "1536px",
+    },
     extend: {},
   },
   plugins: [],
 }
-
